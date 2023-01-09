45afc8a3d945ce76695da3700ed143bb4e204886
Eduardo <edteixeira28@hotmail.com>
2023-01-08 22:09:52 -0300

    Major review on the code

--- models/CpModels.py
+++ models/CpModels.py
@@ -1,8 +1,8 @@
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
-class TSP_CP_Model(object):
-    def __init__(self, c, points=None, dist_fun=None, scaler=None):
+class Matrix_TSP_CP_Model(object):
+    def __init__(self, c):
         self.c = c
         self.route = []
         self.routeList = []
@@ -10,29 +10,27 @@ class TSP_CP_Model(object):
         self.manager = pywrapcp.RoutingIndexManager(len(self.c), 1, 0)
         self.routing = pywrapcp.RoutingModel(self.manager)
 
-        self.transit_callback_index = self.routing.RegisterTransitCallback(self.distance_callback)
+        self.transitCallbackIndex = self.routing.RegisterTransitCallback(self.distanceCallback)
 
-        self.routing.SetArcCostEvaluatorOfAllVehicles(self.transit_callback_index)
+        self.routing.SetArcCostEvaluatorOfAllVehicles(self.transitCallbackIndex)
 
-        self.search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-        self.search_parameters.first_solution_strategy = (
+        self.searchParameters = pywrapcp.DefaultRoutingSearchParameters()
+        self.searchParameters.first_solution_strategy = (
             routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
         )
 
-    def solve(self, time=None, log=False, sol_log=False):
-        if(time != None):
-            self.search_parameters.time_limit.seconds = time
-        self.solution = self.routing.SolveWithParameters(self.search_parameters)
+    def solve(self, totalRuntime=None, printSolutionLog=False, sol_log=False):
+        if(totalRuntime != None):
+            self.searchParameters.time_limit.seconds = totalRuntime
+        self.solution = self.routing.SolveWithParameters(self.searchParameters)
 
         if self.solution:
             self.updateRouteAndRouteList()
 
-    def distance_callback(self, from_index, to_index):
-        from_node = self.manager.IndexToNode(from_index)
-        to_node = self.manager.IndexToNode(to_index)
-        return self.c[from_node][to_node]
+    def distanceCallback(self, from_index, to_index):
+        return self.c[self.manager.IndexToNode(from_index)][self.manager.IndexToNode(to_index)]
 
-    def print_solution(self):
+    def printSolution(self):
         print('Objective Value: {}'.format(self.solution.ObjectiveValue()))
         index = self.routing.Start(0)
         plan_output = 'Route built:\n\n'
@@ -72,4 +70,3 @@ class TSP_CP_Model(object):
             self.route.append((self.manager.IndexToNode(index), self.manager.IndexToNode(new_index)))
             self.routeList.append(self.manager.IndexToNode(new_index))
             index = new_index
-            
