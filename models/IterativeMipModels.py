d19ff524897ba5ec736874fc29e087cb9a8c1407
Eduardo <edteixeira28@hotmail.com>
2023-01-09 18:47:21 -0300

    Major review on the code

--- models/IterativeMipModels.py
+++ models/IterativeMipModels.py
@@ -1,22 +1,22 @@
 import mip
 import time
 from networkx import simple_cycles, DiGraph
-from BasicMipModels import TspBaseModel
+from BasicMipModels import MatrixTspBaseModel
 
-class Iterative_TSP_DFJ_Model(TspBaseModel):
+class Matrix_Iterative_TSP_DFJ_Model(MatrixTspBaseModel):
     """Class to instantiate an iterative solution TSP model based on the classic DFJ formulation."""
-    def __init__(self, c, points=None, dist_fun=None, relax=False, solver="CBC"):
-        TspBaseModel.__init__(self, c, points, dist_fun, relax, solver)
+    def __init__(self, c, relax_X_vars=False, solver="CBC"):
+        MatrixTspBaseModel.__init__(self, c, relax_X_vars, solver)
 
-        self.routes_with_weights = list()
-        self.time_module = time
+        self.routesWithWeights = list()
+        self.G = DiGraph()
 
-    def solve(self, time=None, heur=None, log=False, DFJ_log=True):
-        if(time != None):
-            self.model.max_seconds = time
+    def solve(self, totalRuntime=None, heur=None, printSolverLog=False, printIterationsLog=True):
+        if(totalRuntime != None):
+            self.model.max_seconds = totalRuntime
         if(heur != None):
             pass
-        if(log):
+        if(printSolverLog):
             self.model.verbose = 1
         else:
             self.model.verbose = 0
@@ -25,17 +25,17 @@ class Iterative_TSP_DFJ_Model(TspBaseModel):
         self.totalIter = 1
 
         while True:
-            self.start_solving_time = self.time_module.time()
+            self.start_solving_time = time.time()
             self.model.optimize()
-            self.totalSolutionTime += (self.time_module.time() - self.start_solving_time)
-            self.routes_with_weights = [(i, j, self.c[i, j]) for (i, j) in self.A if self.x[i, j].x > 0.5]
-            if(DFJ_log):
+            self.totalSolutionTime += (time.time() - self.start_solving_time)
+            self.routesWithWeights = [(i, j, self.c[i, j]) for (i, j) in self.A if self.x[i, j].x > 0.5]
+            if(printIterationsLog):
                 print('Iteration {}:'.format(self.totalIter))
                 print("\tPresent objective value: {}".format(self.model.objective_value))
                 print("\tTotal solution time: {}s".format(self.totalSolutionTime))
 
-            self.G = DiGraph()
-            self.G.add_weighted_edges_from(self.routes_with_weights)
+            self.G.clear()
+            self.G.add_weighted_edges_from(self.routesWithWeights)
 
             self.sub_circuits = list(simple_cycles(self.G))
             
@@ -43,8 +43,8 @@ class Iterative_TSP_DFJ_Model(TspBaseModel):
                 self.updateRoute()
                 self.updateRouteList()
                 return
-            if(time != None):
-                if(self.totalSolutionTime > time):
+            if(totalRuntime != None):
+                if(self.totalSolutionTime > totalRuntime):
                     try:
                         (self.x[self.A[0]].x <= 2) == True
                     except:
