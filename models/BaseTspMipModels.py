ea9aed3b56b4fa16abdc1efacfeb6f06192c54bc
Eduardo <edteixeira28@hotmail.com>
2023-01-15 19:05:18 -0300

    Added the MatrixTspIterativeMipBaseModel class

--- models/BaseTspMipModels.py
+++ models/BaseTspMipModels.py
@@ -1,4 +1,6 @@
 import mip
+import time
+from networkx import simple_cycles, DiGraph
 
 class CommonTspMipBaseModel(object):
     """Class to instantiate the common \"Base TSP\" model, that is, a binary assignment model,
@@ -106,6 +108,60 @@ class MatrixTspMipBaseModel(CommonTspMipBaseModel):
 
         # Objective Function
         self.model.objective = mip.xsum(self.c[i][j] * self.x[i, j] for (i, j) in self.A)
+
+class MatrixTspIterativeMipBaseModel(MatrixTspMipBaseModel):
+    def __init__(self, c, relax_X_vars=False, solver="CBC"):
+        MatrixTspMipBaseModel.__init__(self, c, relax_X_vars, solver)
+
+    def solve(self, totalRuntime=None, heur=None, printSolverLog=False, printIterationsLog=True):
+        if(totalRuntime != None):
+            self.model.max_seconds = totalRuntime
+        else:
+            totalRuntime = mip.constants.INF
+        if(heur != None):
+            pass
+        if(printSolverLog):
+            self.model.verbose = 1
+        else:
+            self.model.verbose = 0
+
+        self.totalSolutionTime = 0
+        self.totalIter = 1
+
+        problemGraph = DiGraph()
+
+        while True:
+            start_solving_time = time.time()
+            self.model.max_seconds = totalRuntime - self.totalSolutionTime
+            self.model.optimize()
+            self.totalSolutionTime += (time.time() - start_solving_time)
+            routesWithWeights = [(i, j, self.c[i][j]) for (i, j) in self.A if self.x[i, j].x > 0.5]
+            if(printIterationsLog):
+                print('Iteration {}:'.format(self.totalIter))
+                print("\tPresent objective value: {}".format(self.model.objective_value))
+                print("\tTotal solution time: {}s".format(self.totalSolutionTime))
+
+            problemGraph.clear()
+            problemGraph.add_weighted_edges_from(routesWithWeights)
+
+            self.sub_circuits = list(simple_cycles(problemGraph))
+            
+            if(len(self.sub_circuits)) == 1:
+                self.updateSolution()
+                return
+            if(totalRuntime != None):
+                if(self.totalSolutionTime > totalRuntime):
+                    try:
+                        (self.x[self.A[0]].x <= 2) == True
+                    except:
+                        return
+                    self.updateSolution()
+                    return
+
+            for cycle in self.sub_circuits:
+                self.model += (mip.xsum(self.x[i, j] for i in cycle for j in cycle if (i != j)) <= len(cycle) - 1)
+
+            self.totalIter += 1
 
 class MatrixTspMtzBaseModel(MatrixTspMipBaseModel):
     """Class to instantiate TSP models based on the classic Miller-Tucker-Zemlin, 1960 (MTZ) 
--- models/IterativeMipModels.py
+++ models/IterativeMipModels.py
@@ -1,60 +1,7 @@
-import mip
-import time
-from networkx import simple_cycles, DiGraph
-from BaseTspMipModels import MatrixTspMipBaseModel
+from BaseTspMipModels import MatrixTspIterativeMipBaseModel
 
-class Matrix_Iterative_TSP_DFJ_Model(MatrixTspMipBaseModel):
+class Matrix_Iterative_TSP_DFJ_Model(MatrixTspIterativeMipBaseModel):
     """Class to instantiate an iterative solution TSP model based on the classic 
     Dantzig-Fulkerson-Johnson, 1954 (DFJ) formulation."""
     def __init__(self, c, relax_X_vars=False, solver="CBC"):
-        MatrixTspMipBaseModel.__init__(self, c, relax_X_vars, solver)
-
-    def solve(self, totalRuntime=None, heur=None, printSolverLog=False, printIterationsLog=True):
-        if(totalRuntime != None):
-            self.model.max_seconds = totalRuntime
-        else:
-            totalRuntime = mip.constants.INF
-        if(heur != None):
-            pass
-        if(printSolverLog):
-            self.model.verbose = 1
-        else:
-            self.model.verbose = 0
-
-        self.totalSolutionTime = 0
-        self.totalIter = 1
-
-        problemGraph = DiGraph()
-
-        while True:
-            start_solving_time = time.time()
-            self.model.max_seconds = totalRuntime - self.totalSolutionTime
-            self.model.optimize()
-            self.totalSolutionTime += (time.time() - start_solving_time)
-            routesWithWeights = [(i, j, self.c[i][j]) for (i, j) in self.A if self.x[i, j].x > 0.5]
-            if(printIterationsLog):
-                print('Iteration {}:'.format(self.totalIter))
-                print("\tPresent objective value: {}".format(self.model.objective_value))
-                print("\tTotal solution time: {}s".format(self.totalSolutionTime))
-
-            problemGraph.clear()
-            problemGraph.add_weighted_edges_from(routesWithWeights)
-
-            self.sub_circuits = list(simple_cycles(problemGraph))
-            
-            if(len(self.sub_circuits)) == 1:
-                self.updateSolution()
-                return
-            if(totalRuntime != None):
-                if(self.totalSolutionTime > totalRuntime):
-                    try:
-                        (self.x[self.A[0]].x <= 2) == True
-                    except:
-                        return
-                    self.updateSolution()
-                    return
-
-            for cycle in self.sub_circuits:
-                self.model += (mip.xsum(self.x[i, j] for i in cycle for j in cycle if (i != j)) <= len(cycle) - 1)
-
-            self.totalIter += 1
+        MatrixTspIterativeMipBaseModel.__init__(self, c, relax_X_vars, solver)
